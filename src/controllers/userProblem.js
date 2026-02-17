faa19ad6d39da17c2d51290ae27d48b288158943
manish kumar Sharma <manishsharma830245@gmail.com>
2026-02-17 18:56:46 +0530

    Stable version before self-hosting Judge0

--- src/controllers/userProblem.js
+++ src/controllers/userProblem.js
@@ -1,5 +1,5 @@
 const {submitBatch,getLanguageById} =require("../utils/ProblemUtility")
-
+const Problem = require("../models/problem")
 const createProblem =async (req,res) => {
     //problem.js will give title,des,diffiulty,tags to it  when user press submit
     const {title,description,difficulty,tags,visibleTestCases,hiddenTestCases,startCode,
@@ -12,17 +12,32 @@ const createProblem =async (req,res) => {
             const languageId =getLanguageById(language);
             //we need to make batch submission array
             //visibleTestCases =[{input:"ded" ,output:" efe",explanantion:" efuew"},{input:"ded" ,output:" efe",explanantion:" efuew"}]
-            const submissions =visibleTestCases.map((input,output)=>({
+            const submissions =visibleTestCases.map((testcase)=>({
                 source_code:completeCode, //code written by user on leetcode
                 language_id: languageId,  //selected c++ on leetcode
-                stdin: input,    // judge0 question given
-                expected_output:output //judge0 correct answer
+                stdin: testcase.input,    // judge0 question given
+                expected_output: testcase.output //judge0 correct answer
             }));  // submission array of c++,java having batch of testcase each created =[{language:id,source_code:code,stdin:,output:}, {language:id,source_code:code}....]
         }
         const submitResult =await submitBatch(submissions);
 
+        const resulttoken =submitResult.map((value)=> value.token); //submit result=[{"token":" efef"},{"token":"fwf"},{"token":"gfe"}]
+    //  result token ={"ffe","fre","frfr"}
+    const testResult = await submitToken(resultToken);
+    for(const test of testResult){
+        if(test.status_id!=3){ return res.status(400).send("error occured"); } //return cause i want so this function dot run again
+    }
+    //we can store it in our db(dont put in for loop when everything settles then store)
+    await  Problem.create({
+        ...req.body,
+        problemCreator:req.result._id //in line 23 of adminMidlleware.js i inserted result in req.result
+    });
+    
+    res.status(201).send("problem saved success");
+
     }
     catch(err){
+        res.status(400).send("err"+err);
 
     }
 }
--- src/utils/ProblemUtility.js
+++ src/utils/ProblemUtility.js
@@ -1,46 +1,84 @@
-const axios =require('axios');
+const axios = require("axios");
 
-const getLanguageById =(lang)=>{
-    const language = {
-        "c++":54,
-        "java":62,
-        "javascript":63
+const getLanguageById = (lang) => {
+  const language = {
+    "c++": 54,
+    java: 62,
+    javascript: 63,
+  };
+  return language[lang.toLowerCase()];
+};
+
+const submitBatch = async (submissions) => {
+  const options = {
+    method: "POST",
+    url: "https://judge0-ce.p.rapidapi.com/submissions/batch",
+    params: {
+      base64_encoded: "true",
+    },
+    headers: {
+      "x-rapidapi-key": "YOUR_RAPIDAPI_KEY",
+      "x-rapidapi-host": "judge0-ce.p.rapidapi.com",
+      "Content-Type": "application/json",
+    },
+    data: {
+      submissions,
+    },
+  };
+
+  async function fetchData() {
+    try {
+      const response = await axios.request(options);
+      return response.data;
+    } catch (error) {
+      console.error(error);
     }
-    return language[lang.toLowerCase()];
+  }
+
+  fetchData();
+};
+
+const waiting =async(timer)=>{
+    setTimeout(()=>{
+        return 1;
+    },timer);
 }
-
-
-const submitBatch =async (submissions)=>{
-
-     const options = {
-     method: 'POST',
-     url: 'https://judge0-ce.p.rapidapi.com/submissions/batch',
-     params: {
-     base64_encoded: 'true'
-             },
-     headers: {
-      'x-rapidapi-key': 'YOUR_RAPIDAPI_KEY',
-      'x-rapidapi-host': 'judge0-ce.p.rapidapi.com',
-      'Content-Type': 'application/json'
-          },
-     data: {
-        submissions
-           }
-     };
+const submitToken = async (resultToken) => {
+  const axios = require("axios");
+  const submitToken = async (resultToken) => {
+    const options = {
+      method: "GET",
+      url: "https://judge0-ce.p.rapidapi.com/submissions/batch",
+      params: {
+        tokens: resultToken.join(","),
+        base64_encoded: "true",
+        fields: "*",
+      },
+      headers: {
+        "x-rapidapi-key": "ab99c6ec42mshfd636ec7c6687efp1b9043jsna684835b0591",
+        "x-rapidapi-host": "judge0-ce.p.rapidapi.com",
+      },
+    };
 
     async function fetchData() {
-         try {
-              const response = await axios.request(options);
-              return response.data;
-             } catch (error) {
-               console.error(error);
-             }
+      try {
+        const response = await axios.request(options);
+        return response.data;
+      } catch (error) {
+        console.error(error);
+      }
     }
 
-    fetchData();
-
-
-}
-
-module.exports = {getLanguageById,submitBatch};
-
+    while (true) {
+      const result = await fetchData();
+      const IsResultObtained = result.submissions.every((r) => {
+        r.status_id > 3;
+      });
+      if (IsResultObtained) {
+        return result.submissions;
+      }
+      await waiting(1000); //lets wait for 1 sec before calling function again
+    }
+  };
+};
+module.exports = { getLanguageById, submitBatch, submitToken };
