27ff58e2308d0290ef238acace1f688e6d932edd
manish kumar Sharma <manishsharma830245@gmail.com>
2026-02-18 18:45:29 +0530

    added problem -update,delete,gellall etc..

--- src/controllers/userProblem.js
+++ src/controllers/userProblem.js
@@ -1,5 +1,6 @@
 const {submitBatch,getLanguageById} =require("../utils/ProblemUtility")
-const Problem = require("../models/problem")
+const Problem = require("../models/problem");
+//------------------------------------------------------------------------------
 const createProblem =async (req,res) => {
     //problem.js will give title,des,diffiulty,tags to it  when user press submit
     const {title,description,difficulty,tags,visibleTestCases,hiddenTestCases,startCode,
@@ -18,17 +19,18 @@ const createProblem =async (req,res) => {
                 stdin: testcase.input,    // judge0 question given
                 expected_output: testcase.output //judge0 correct answer
             }));  // submission array of c++,java having batch of testcase each created =[{language:id,source_code:code,stdin:,output:}, {language:id,source_code:code}....]
-        }
+        
         const submitResult =await submitBatch(submissions);
 
-        const resulttoken =submitResult.map((value)=> value.token); //submit result=[{"token":" efef"},{"token":"fwf"},{"token":"gfe"}]
+        const resultToken =submitResult.map((value)=> value.token); //submit result=[{"token":" efef"},{"token":"fwf"},{"token":"gfe"}]
     //  result token ={"ffe","fre","frfr"}
-    const testResult = await submitToken(resultToken);
-    for(const test of testResult){
-        if(test.status_id!=3){ return res.status(400).send("userproblem in createproblem error:"); } //return cause i want so this function dot run again
+        const testResult = await submitToken(resultToken);
+        for(const test of testResult){
+          if(test.status_id!=3){ return res.status(400).send("userproblem in createproblem error:"); } //return cause i want so this function dot run again
     }
+}
     //we can store it in our db(dont put in for loop when everything settles then store)
-    await  Problem.create({
+    const userProblem =await  Problem.create({
         ...req.body,
         problemCreator:req.result._id //in line 23 of adminMidlleware.js i inserted result in req.result
     });
@@ -42,4 +44,87 @@ const createProblem =async (req,res) => {
     }
 }
 
-module.exports =createProblem;
+//-------------------------------------------------------------------------------------------------
+
+const updateProblem = async (req,res)=>{
+  const {id} = req.params;
+  const {title,description,difficulty,tags,visibleTestCases,hiddenTestCases,startCode,referenceSolution, 
+    problemCreator} = req.body;
+  try{
+    if(!id){ return res.status(400).send("Missing ID Field in updateproblem"); }//put return so if it occur fucntion dont go further
+    const DsaProblem =  await Problem.findById(id);
+    if(!DsaProblem){ return res.status(404).send("ID is not persent in server"); }
+
+    for(const {language,completeCode} of referenceSolution){
+      const languageId = getLanguageById(language);
+      const submissions = visibleTestCases.map((testcase)=>({
+          source_code:completeCode,
+          language_id: languageId,
+          stdin: testcase.input,
+          expected_output: testcase.output
+      }));
+      const submitResult = await submitBatch(submissions);
+      const resultToken = submitResult.map((value)=> value.token);
+      const testResult = await submitToken(resultToken);
+     for(const test of testResult){
+      if(test.status_id!=3){ return res.status(400).send("Error Occured");}
+     }
+
+    }
+    const newProblem = await Problem.findByIdandUpdate(id,{...req.body},{runValidators:true, new:true})
+    res.status(200).send(newProblem);
+}
+catch(err){"updateproblem error"+err}
+}
+//------------------------------------------------------------------------------------------------------
+const deleteProblem =async (req,res)=>{
+    const {id}=req.params;
+    try{
+        if(!id){return res.status(400).send("id missing in updateProblem")}
+        const deletedProblem = await Problem.findByIdAndDelete(id);
+        if(!deletedProblem){ return res.status(404).send("problem is missing in deleteProblem")}
+        res.status(200).send("successfully deleted");
+
+    }
+    catch(err){
+        res.status(500).send("errror"+err);       
+    }
+}
+
+//--------------------------------------------------------------------------------
+
+const getProblemById =async (req,res)=>{
+    const {id}=req.params;
+    try{
+        if(!id){return res.status(400).send("id misiing in updateProblem")}
+        const getProblem = await Problem.findById(id);
+        if(!getProblem){ return res.status(404).send("problem is missing in getProblemBYid")}
+        res.status(200).send(getProblem);
+
+    }
+    catch(err){
+        res.status(500).send("eror in getProbleminID"+err);       
+    }
+}
+
+//------------------------------------------------------------------------------------------------
+
+const getAllProblem =async (req,res)=>{
+    try{
+        const getProblem = await Problem.find({});   //gives array
+        if(getProblem.length==0){ return res.status(404).send("not a single problem is present in server")}
+        res.status(200).send(getProblem);
+
+    }
+    catch(err){
+        res.status(500).send("error in getAllProblem"+err);       
+    }  
+}
+
+//---------------------------------------------------------------------------------------
+
+const solvedAllProblemByUser =(req,res)=>{
+    
+}
+
+module.exports ={createProblem,updateProblem,deleteProblem,getProblemById,getAllProblem,solvedAllProblemByUser}
--- src/routes/problemCreator.js
+++ src/routes/problemCreator.js
@@ -2,15 +2,17 @@
 const express= require('express');
 const problemRouter = express.Router();
 const adminMiddleware =require("../middleware/adminMiddleware");
-const createProblem =require("../controllers/userProblem")
+const userMiddleware =require("../middleware/userMiddleware");
+const {createProblem,updateProblem,deleteProblem,getProblemById,getAllProblem,solvedAllProblemByUser} =require("../controllers/userProblem");
+
 //create
 problemRouter.post("/create",adminMiddleware, createProblem);
-// problemRouter.patch("/update/:id",updateProblem);
-// problemRouter.delete("/delete/:id",deleteProblem);
+problemRouter.put("/update/:id",adminMiddleware,updateProblem);
+problemRouter.delete("/delete/:id",adminMiddleware,deleteProblem);
 
-// problemRouter.get("/problemById/:id",getProblemById);
-// problemRouter.get("/getAllProblem",getAllProblem);
-// problemRouter.get("/problemSolvedByUser", solvedAllProblemByUser);
+problemRouter.get("/problemById/:id",userMiddleware,getProblemById);
+problemRouter.get("/getAllProblem",getAllProblem);
+problemRouter.get("/problemSolvedByUser", solvedAllProblemByUser);
 
 module.exports =problemRouter;
 //probelm fetch
